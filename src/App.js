553978f58d70cf0fb0ed2cf682cc417a678efbde
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2018-01-05 13:48:04 +0530

    push more updates

--- src/App.js
+++ src/App.js
@@ -134,7 +134,7 @@ class TeamList extends Component {
         const avatar = member.avatar_url + '&s=30';
         teamListContainer.push(
           <li key={key}>
-            <Link to={`/u/${member.id}`}><img src={avatar} className="avatar-image"></img>{member.login}</Link>
+            <Link to={`/u/${member.id}`}><img src={avatar} className="avatar-image" alt={member.id}></img>{member.login}</Link>
           </li>
         )
       });
@@ -151,15 +151,14 @@ class TeamList extends Component {
 class UserDetail extends Component {
   render() {
     const user = this.props.user;
+    const hireable = user.hireable ? 'Yes': 'No';
     console.log('userdetail', user);
     return(
       <div>
         <h1>
           Hello {user.login}
-        </h1>
-        <p>
-          Bio: {user.bio}
-        </p>
+        </h1>  
+        { user.bio && <p>Bio: {user.bio} </p> }
         <img src={user.avatar_url} alt={user.id}>
         </img>
         <p>
@@ -169,7 +168,7 @@ class UserDetail extends Component {
         { user.company && <p>Company: {user.company}</p> }
         { user.blog && <p>Blog: <a href={user.blog}>{user.blog}</a></p> }
         { user.location && <p>Location: {user.location}</p> }
-        { user.hireable && <p>Open for Job: {user.hireable}</p> }
+        { user.hireable && <p>Open for Job: {hireable}</p> }
       </div>
     );
   }
